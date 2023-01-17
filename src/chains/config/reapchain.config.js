4b9ceb29f2e0a03af77272236732d024e7e52a2b
nannarae <awsed333@reappay.co.kr>
2023-01-17 14:29:15 +0900

    fix : change chain info for mainnet-metamask

--- src/chains/config/reapchain.config.js
+++ src/chains/config/reapchain.config.js
@@ -1,38 +1,40 @@
-export const chainInfo = {
-  env: "main",
-  chainName: "reapchain",
-  chainId: "2022",
-  cosmosChainId: "reapchain_2022-1",
-  restEndpoint: "https://lcd.reapchain.org",
-  rpcEndpoint: "https://rpc.reapchain.org",
-  evmEndpoint: "https://eth.reapchain.org",
-  stateSyncEndpoint: [
-    "https://state-sync-rpc.reapchain.org:443",
-    "https://rpc-network.reapchain.org:443",
-  ],
-  stateSyncP2P: "state-sync-p2p.reapchain.org:27100",
-  stateSyncNodeId: "090522bf5a8e939553d8fe9257a6350898f697cf",
-  dashboardUrl: `https://dashboard.reapchain.org/validators`,
-  ethAccountExplorerUrl: `https://dashboard.reapchain.org/account`,
-};
-
 // export const chainInfo = {
-//   env: "test",
+//   env: "main",
 //   chainName: "reapchain",
 //   chainId: "2022",
-//   cosmosChainId: "mercury_2022-3",
-//   restEndpoint: "https://test-lcd.reapchain.org",
-//   rpcEndpoint: "https://test-rpc.reapchain.org",
-//   evmEndpoint: "https://test-eth.reapchain.org",
+//   chainIdHex: "0x7e6",
+//   cosmosChainId: "reapchain_2022-1",
+//   restEndpoint: "https://lcd.reapchain.org",
+//   rpcEndpoint: "https://rpc.reapchain.org",
+//   evmEndpoint: "https://eth.reapchain.org",
 //   stateSyncEndpoint: [
-//     "https://test-state-sync-rpc.reapchain.org:443",
-//     "https://test-rpc-network.reapchain.org:443",
+//     "https://state-sync-rpc.reapchain.org:443",
+//     "https://rpc-network.reapchain.org:443",
 //   ],
-//   stateSyncP2P: "13.125.20.241:27102",
-//   stateSyncNodeId: "05ec7232feb9f0e8af8c2b5e98411043176995b0",
-//   dashboardUrl: `https://test-dashboard.reapchain.org/staking`,
-//   ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
+//   stateSyncP2P: "state-sync-p2p.reapchain.org:27100",
+//   stateSyncNodeId: "090522bf5a8e939553d8fe9257a6350898f697cf",
+//   dashboardUrl: `https://dashboard.reapchain.org/validators`,
+//   ethAccountExplorerUrl: `https://dashboard.reapchain.org/account`,
 // };
+
+export const chainInfo = {
+  env: "test",
+  chainName: "reapchain",
+  chainId: "2022",
+  chainIdHex: "0x7e6",
+  cosmosChainId: "mercury_2022-3",
+  restEndpoint: "https://test-lcd.reapchain.org",
+  rpcEndpoint: "https://test-rpc.reapchain.org",
+  evmEndpoint: "https://test-eth.reapchain.org",
+  stateSyncEndpoint: [
+    "https://test-state-sync-rpc.reapchain.org:443",
+    "https://test-rpc-network.reapchain.org:443",
+  ],
+  stateSyncP2P: "13.125.20.241:27102",
+  stateSyncNodeId: "05ec7232feb9f0e8af8c2b5e98411043176995b0",
+  dashboardUrl: `https://test-dashboard.reapchain.org/staking`,
+  ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
+};
 
 export const networkInfo = {
   chain_name: chainInfo.chainName,
