0e6a2cd355d34faffb451e64870ec38235fa20e4
nannarae <awsed333@reappay.co.kr>
2023-07-13 12:20:43 +0900

    env : change chainid

--- src/chains/config/reapchain.config.js
+++ src/chains/config/reapchain.config.js
@@ -1,9 +1,9 @@
 export const chainInfo = {
   env: "test",
   chainName: "reapchain",
-  chainId: "2022",
+  chainId: "2023",
   chainIdHex: "0x7e6",
-  cosmosChainId: "mercury_2022-3",
+  cosmosChainId: "mercury_2023-1",
   restEndpoint: "https://test-lcd.reapchain.org",
   rpcEndpoint: "https://test-rpc.reapchain.org",
   evmEndpoint: "https://test-eth.reapchain.org",
@@ -17,25 +17,6 @@ export const chainInfo = {
   ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
 };
 
-// export const chainInfo = {
-//   env: "test",
-//   chainName: "reapchain",
-//   chainId: "2022",
-//   chainIdHex: "0x7e6",
-//   cosmosChainId: "mercury_2022-3",
-//   restEndpoint: "http://192.168.100.39:1317",
-//   rpcEndpoint: "http://192.168.100.39:27000",
-//   evmEndpoint: "http://192.168.100.39:27400",
-//   stateSyncEndpoint: [
-//     "https://test-state-sync-rpc.reapchain.org:443",
-//     "https://test-rpc-network.reapchain.org:443",
-//   ],
-//   stateSyncP2P: "13.125.20.241:27102",
-//   stateSyncNodeId: "05ec7232feb9f0e8af8c2b5e98411043176995b0",
-//   dashboardUrl: `https://test-dashboard.reapchain.org/staking`,
-//   ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
-// };
-
 export const networkInfo = {
   chain_name: chainInfo.chainName,
   coingecko: "",
@@ -44,7 +25,7 @@ export const networkInfo = {
   snapshot_provider: "",
   sdk_version: "0.45.1",
   coin_type: "60",
-  min_tx_fee: "15000000",
+  min_tx_fee: "31250000000000",
   addr_prefix: "reap",
   logo: "/logos/reapchain_logo.png",
   assets: [
