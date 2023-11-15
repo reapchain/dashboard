c7516978026bc597b797e2fa4357840dc0e1d9e4
nannarae <awsed333@reappay.co.kr>
2023-11-15 22:35:54 +0900

    chore: overwrite config file

--- src/chains/config/reapchain.config.js
+++ src/chains/config/reapchain.config.js
@@ -1,24 +1,3 @@
-// export const chainInfo = {
-//   env: "main",
-//   chainName: "reapchain",
-//   chainId: "221230",
-//   chainIdHex: "0x3602E",
-//   cosmosChainId: "reapchain_221230-1",
-//   restEndpoint: "https://lcd.reapchain.org",
-//   rpcEndpoint: "https://rpc.reapchain.org",
-//   evmEndpoint: "https://eth.reapchain.org",
-//   stateSyncEndpoint: [
-//     "https://state-sync-rpc.reapchain.org:443",
-//     "https://rpc-network.reapchain.org:443",
-//   ],
-//   stateSyncP2P: "state-sync-p2p.reapchain.org:27100",
-//   stateSyncNodeId: "e680439ea3ce99f020cc3250084b2cac61ac0042",
-//   // snapshotInterval: 1200,
-//   // snapshotKeepRecent: 10,
-//   dashboardUrl: `https://dashboard.reapchain.org/validators`,
-//   ethAccountExplorerUrl: `https://dashboard.reapchain.org/account`,
-// };
-
 export const chainInfo = {
   env: "test",
   chainName: "Reapchain Testnet",
@@ -37,44 +16,6 @@ export const chainInfo = {
   dashboardUrl: `https://test-dashboard.reapchain.org/staking`,
   ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
 };
-
-// export const chainInfo = {
-//   env: "bridge-test",
-//   chainName: "Reapchain Testnet",
-//   chainId: "2023",
-//   chainIdHex: "0x7e7",
-//   cosmosChainId: "mercury_2023-1",
-//   restEndpoint: "http://43.201.57.7:1317",
-//   rpcEndpoint: "http://43.201.57.7:27000",
-//   evmEndpoint: "http://43.201.57.7:27400",
-//   stateSyncEndpoint: [
-//     "https://test-state-sync-rpc.reapchain.org:443",
-//     "https://test-rpc-network.reapchain.org:443",
-//   ],
-//   stateSyncP2P: "13.125.20.241:27102",
-//   stateSyncNodeId: "05ec7232feb9f0e8af8c2b5e98411043176995b0",
-//   dashboardUrl: `https://test-dashboard.reapchain.org/staking`,
-//   ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
-// };
-
-// export const chainInfo = {
-//   env: "local",
-//   chainName: "Reapchain Local",
-//   chainId: "221231",
-//   chainIdHex: "0x3602F",
-//   cosmosChainId: "reapchain_221231-1",
-//   restEndpoint: "http://192.168.100.4:1317",
-//   rpcEndpoint: "http://192.168.100.4:27000",
-//   evmEndpoint: "http://192.168.100.4:27400",
-//   stateSyncEndpoint: [
-//     "https://test-state-sync-rpc.reapchain.org:443",
-//     "https://test-rpc-network.reapchain.org:443",
-//   ],
-//   stateSyncP2P: "13.125.20.241:27102",
-//   stateSyncNodeId: "05ec7232feb9f0e8af8c2b5e98411043176995b0",
-//   dashboardUrl: `https://test-dashboard.reapchain.org/staking`,
-//   ethAccountExplorerUrl: `https://test-dashboard.reapchain.org/account`,
-// };
 
 export const networkInfo = {
   chain_name: chainInfo.chainName,
