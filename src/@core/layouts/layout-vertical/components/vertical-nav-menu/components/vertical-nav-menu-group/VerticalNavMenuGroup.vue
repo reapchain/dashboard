9604b3d6a42522f4505d5cf7b0d8303971cbb387
nannarae <awsed333@reappay.co.kr>
2024-03-04 15:01:51 +0900

    feat: add dropdown to provide links to different versions

--- src/@core/layouts/layout-vertical/components/vertical-nav-menu/components/vertical-nav-menu-group/VerticalNavMenuGroup.vue
+++ src/@core/layouts/layout-vertical/components/vertical-nav-menu/components/vertical-nav-menu-group/VerticalNavMenuGroup.vue
@@ -8,30 +8,23 @@
       'sidebar-group-active': isActive,
     }"
   >
-    <b-link
-      class="d-flex align-items-center"
-      @click="() => updateGroupOpen(!isOpen)"
+    <b-dropdown
+      :text="versionName"
+      :variant="item.tagVariant || 'primary'"
+      class="d-flex align-items-center custom-dropdown"
+      right
     >
-      <b-avatar
-        variant="transparent"
-        :src="item.icon"
-        icon="people-fill"
-        size="sm"
-        class="mr-1"
-      />
-
-      <span class="menu-title text-truncate text-uppercase">{{
-        item.title
-      }}</span>
-      <b-badge
-        v-if="item.tag"
-        pill
-        :variant="item.tagVariant || 'primary'"
-        class="mr-1 ml-auto"
+      <b-dropdown-item
+        v-for="item in dropdownItems"
+        :key="item.version"
+        class="custom-dropdown-item"
+        :href="item.link"
+        target="_blank"
       >
-        {{ item.tag }}
-      </b-badge>
-    </b-link>
+        {{ item.name }}
+      </b-dropdown-item>
+    </b-dropdown>
+
     <b-collapse v-model="isOpen" class="menu-content" tag="ul">
       <component
         :is="resolveNavItemComponent(child)"
@@ -44,8 +37,43 @@
   </li>
 </template>
 
+<style scoped>
+.custom-dropdown {
+  background-color: linear-gradient(118deg, #00264c, rgba(0, 38, 76, 0.7));
+  border: 1px solid #ccc;
+  size: 25px;
+  margin: 15px; /* Add your desired margin value */
+  /* ... add more styles as needed */
+  margin-bottom: 12px;
+}
+
+.dropdown-toggle {
+  size: 55px;
+}
+
+.custom-dropdown-item {
+  /* Your custom styles here */
+  background-color: #f0f0f0;
+  /* size: 100px; */
+  margin: 10px; /* Add your desired margin value */
+  /* ... add more styles as needed */
+
+  a {
+    width: 208px;
+    text-align: center;
+  }
+}
+</style>
+
 <script>
-import { BLink, BBadge, BCollapse, BAvatar } from "bootstrap-vue";
+import {
+  BLink,
+  BBadge,
+  BCollapse,
+  BAvatar,
+  BDropdown,
+  BDropdownItem,
+} from "bootstrap-vue";
 import { resolveVerticalNavMenuItemComponent as resolveNavItemComponent } from "@core/layouts/utils";
 import { useUtils as useI18nUtils } from "@core/libs/i18n";
 import { useUtils as useAclUtils } from "@core/libs/acl";
@@ -56,6 +84,26 @@ import VerticalNavMenuLink from "../vertical-nav-menu-link/VerticalNavMenuLink.v
 import useVerticalNavMenuGroup from "./useVerticalNavMenuGroup";
 import mixinVerticalNavMenuGroup from "./mixinVerticalNavMenuGroup";
 
+import { chainInfo } from "/env/reapchain.config";
+
+const reapchainVersion = {
+  "v3.1": {
+    version: "v3.1",
+    name: "Reapchain-V3.1",
+    link: "https://dashboard.reapchain.org",
+  },
+  "v3.0": {
+    version: "v3.0",
+    name: "Reapchain-V3.0",
+    link: "https://v3.dashboard.reapchain.org",
+  },
+  "v2.0": {
+    version: "v2.0",
+    name: "Reapchain-V2.0",
+    link: "https://v2.dashboard.reapchain.org",
+  },
+};
+
 export default {
   name: "VerticalNavMenuGroup",
   components: {
@@ -65,6 +113,8 @@ export default {
     BLink,
     BBadge,
     BCollapse,
+    BDropdown,
+    BDropdownItem,
   },
   mixins: [mixinVerticalNavMenuGroup],
   props: {
@@ -72,6 +122,26 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  data() {
+    let dropdownItems = [];
+    let versionName = "";
+
+    if (chainInfo.version === "v3.1" || !chainInfo.version) {
+      dropdownItems = [reapchainVersion["v3.0"], reapchainVersion["v2.0"]];
+      versionName = reapchainVersion["v3.1"].name;
+    } else if (chainInfo.version === "v3.0") {
+      dropdownItems = [reapchainVersion["v3.1"], reapchainVersion["v2.0"]];
+      versionName = reapchainVersion["v3.0"].name;
+    } else if (chainInfo.version === "v2.0") {
+      dropdownItems = [reapchainVersion["v3.1"], reapchainVersion["v3.0"]];
+      versionName = reapchainVersion["v2.0"].name;
+    }
+
+    return {
+      dropdownItems,
+      versionName,
+    };
   },
   setup(props) {
     const {
@@ -98,7 +168,6 @@ export default {
       t,
     };
   },
+  computed: {},
 };
 </script>
-
-<style></style>
